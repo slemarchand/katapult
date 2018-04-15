c9eec02bf6b236917a41989204d07046a3356c72
Sébastien Le Marchand <sebastien.lemarchand@gmail.com>
2018-04-16 01:01:06 +0200

    Improve CLI experience

--- src/main/kotlin/com/slemarchand/katapult/Application.kt
+++ src/main/kotlin/com/slemarchand/katapult/Application.kt
@@ -3,12 +3,14 @@ package com.slemarchand.katapult
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import com.beust.jcommander.JCommander
+import com.beust.jcommander.ParameterException
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.batch.BasicBatchConfigurer
 import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
+import kotlin.system.exitProcess
 
 @SpringBootApplication
 open class Application {
@@ -19,20 +21,27 @@ open class Application {
 
         fun parseArgs(args: Array<String>) {
 
-            JCommander.newBuilder()
+            val jCommander = JCommander.newBuilder()
                     .addObject(parameters)
                     .build()
-                    .parse(*args);
+
+            jCommander.setProgramName("katapult")
+            jCommander.parse(*args);
         }
     }
 }
 
 fun main(args: Array<String>) {
 
-    Application.parseArgs(args);
+    try {
+        Application.parseArgs(args);
 
-    val app = SpringApplication(Application::class.java)
-    app.setLogStartupInfo(false);
-    app.setBannerMode(Banner.Mode.OFF)
-    app.run()
+        val app = SpringApplication(Application::class.java)
+        app.setLogStartupInfo(false)
+        app.setBannerMode(Banner.Mode.OFF)
+        app.run()
+    } catch (e: ParameterException) {
+        println(e.usage())
+        exitProcess(1)
+    }
 }
--- src/main/kotlin/com/slemarchand/katapult/Parameters.kt
+++ src/main/kotlin/com/slemarchand/katapult/Parameters.kt
@@ -4,22 +4,31 @@ import com.beust.jcommander.Parameter
 
 class Parameters {
 
-    @Parameter
+    @Parameter(required = true,
+            description = "<csv-file>")
     var path: String? = null
 
     @Parameter(names = arrayOf("-u", "--user"),
+            required = true,
+            order = 30,
             description = "The user")
     var user: String? = null
 
     @Parameter(names = arrayOf("-p", "--password"),
+            required = true,
+            order = 31,
             description = "The password")
     var password: String? = null
 
     @Parameter(names = arrayOf("-s", "--server"),
+            required = true,
+            order = 10,
             description = "The server base URL ")
     var server: String? = null
 
     @Parameter(names = arrayOf("-c", "--companyId"),
+            required = true,
+            order = 20,
             description = "The company ID")
     var companyId: Long? = null
 
