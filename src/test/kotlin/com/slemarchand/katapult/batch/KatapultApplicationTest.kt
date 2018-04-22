944e4eac4195ae17c1f96138f08d956b13fa271d
Sébastien Le Marchand <sebastien.lemarchand@gmail.com>
2018-04-22 11:48:48 +0200

    Unit testing in progress...

--- src/test/kotlin/com/slemarchand/katapult/batch/KatapultApplicationTest.kt
+++ src/test/kotlin/com/slemarchand/katapult/batch/KatapultApplicationTest.kt
@@ -35,4 +35,30 @@ class KatapultApplicationTest {
 
         } catch (e: ParameterException) {}
     }
+
+    @Test
+    fun testParseArgs_valid_parameters() {
+
+        val args = arrayOf("/some/path",
+                "-c","100034",
+                "-s","http://someserver.com",
+                "-u","user",
+                "-p","pass")
+
+        try {
+
+            KatapultApplication.CommandLine.parseArgs(args)
+
+        } catch (e: ParameterException) {
+            fail<Any>(e)
+        }
+
+        val params = KatapultApplication.parameters
+
+        assertEquals("/some/path", params.path)
+        assertEquals(100034L, params.companyId)
+        assertEquals("http://someserver.com", params.server)
+        assertEquals("user", params.user)
+        assertEquals("pass", params.password)
+    }
 }
